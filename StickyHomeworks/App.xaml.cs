f6ac6f36b1691190991c3543cbcf9f7997209f12
WRC <hello_wrc@outlook.com>
2023-09-15 20:40:58 +0800

    init

---
+++ StickyHomeworks/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using ElysiaFramework;
+using Microsoft.Extensions.Hosting;
+
+namespace StickyHomeworks;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : AppEx
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        Host = Microsoft.Extensions.Hosting.Host.
+            CreateDefaultBuilder().
+            UseContentRoot(AppContext.BaseDirectory).
+            ConfigureServices((context, services) => { }).
+            Build();
+        _ = Host.StartAsync();
+        base.OnStartup(e);
+    }
+}
